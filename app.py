c743e7bd2cc67c09e241487b1b49d7080765703c
Nikita Leev <nikita.leev@gmail.com>
2023-12-25 16:13:31 +0000

    Add docker-compose & env variables support

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print('Hello, tg bot' + os.environ['TELEGRAM_KEY'])
     return "<p>Hello, World!</p>"
 
 
