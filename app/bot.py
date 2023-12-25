643b7daaedb1d5fce9be1390c93c2ca1a27367ba
Nikita Leev <nikita.leev@gmail.com>
2023-12-25 23:16:26 +0000

    Playing around LLM & telegram bot.

---
+++ app/bot.py
@@ -0,0 +1,62 @@
+import os
+
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+
+TOKEN: Final = os.environ['TELEGRAM_KEY']
+BOT_USERNAME: Final = 'KhargolGroBogukBot'
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello there. I'm a Dagon Fel bot")
+
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello there. I'm a Dagon Fel bot")
+
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if 'hello' in processed:
+        return 'hi back'
+    return 'nah'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print('Bot', response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+
+if __name__ == '__main__':
+    print('Starting bot')
+    app = Application.builder().token(TOKEN).build()
+
+    # commands
+    app.add_handler(CommandHandler('start', start_command))
+
+    # messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+
+    print('Polling')
+    app.run_polling(poll_interval=3)
---
+++ llm/example.py
@@ -0,0 +1,38 @@
+import os
+
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+information = """
+Paul Adrien Maurice Dirac OM FRS[6] (/dɪˈræk/; 8 August 1902 – 20 October 1984) was an English theoretical physicist who is considered to be one of the founders of 
+quantum mechanics and quantum electrodynamics. He is credited with laying the foundations of quantum field theory.
+He was the Lucasian Professor of Mathematics at the University of Cambridge, a professor of physics at Florida State University and the University of Miami, 
+and a 1933 Nobel Prize in Physics recipient.
+"""
+
+def get_summary_chain() -> LLMChain:
+    summary_template = """
+         given the information {information} I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+     """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={
+        },
+    )
+
+    return LLMChain(llm=llm, prompt=summary_prompt_template)
+
+if __name__ == "__main__":
+    print(f"""running with key: {OPENAI_API_KEY}""")
+    chain = get_summary_chain()
+    print(chain.run(information=information))
